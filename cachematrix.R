c58424d72d222db8f25726838840bcd59548dac5
Sherry Cao <sherrycn66@gmail.com>
2020-10-15 06:59:14 +0800

    update example

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,7 @@
 ## 3.set the value of the inverse
 ## 4.get the value of the inverse
 
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -22,6 +23,18 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse=getinverse)
   
 }
+## @example:
+##functions <- makeCacheMatrix()
+##functions$set(matrix(1:4, 2))
+##functions$get()
+#     [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+##functions$setInverse()
+##functions$getInverse()
+#     [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
 ###I've researched online and found the above solution is the most straightforward approach.
 #It also can be solved by using the cacheSolve function which computes the inverse of the "matrix
 
